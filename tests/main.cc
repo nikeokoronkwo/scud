203c5d0ec755e05ddd9064dee7c0d05bf5ddd6de
--add <nikechukwu@gmail.com>
2024-06-25 10:14:27 -0400

    test code for now

---
+++ tests/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "scud_ast.h"
+
+using namespace ast;
+
+int main() {
+  AST ast = parse(R"(
+project("scudderia", "0.1.0")
+
+executable("foo", C, 
+  src: ["foo.c"]
+)
+)");
+  std::cout << ast.expressions().size();
+}
