8cc22835a1edd50b1e67b1d52e3ae0651ee5e60a
--add <nikechukwu@gmail.com>
2024-06-25 10:13:34 -0400

    Linking Compiler frontend and API backend

---
+++ src/gen/gen.h
@@ -0,0 +1,19 @@
+#ifndef SCUD_SRC_GEN_GEN_H_
+#define SCUD_SRC_GEN_GEN_H_
+
+#include "src/compiler/parser.h"
+#include "src/"
+
+class ProgramVisitor: public Visitor {
+public:
+  void visit(ast::AST& ast) override;
+  void visit_expr(ast::Expr& expr) override;
+  void visit_string_expr(ast::StringExpr& expr) override;
+  void visit_list_expr(ast::ListExpr& expr) override;
+  void visit_identifier_expr(ast::IdentifierExpr& expr) override;
+  void visit_variable_expr(ast::VariableExpr& expr) override;
+  void visit_parameter_expr(ast::ParameterExpr& expr) override;
+  void visit_function_expr(ast::FunctionExpr& expr) override;
+};
+
+#endif
